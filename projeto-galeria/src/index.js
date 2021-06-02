3f8347b6962a69fb17861969ac5e52a5ab81ac29
Marcos Vinicius Inocencio <marcos.inocencio13@gmail.com>
2021-06-02 15:14:49 -0300

    Projeto Galeria
    Galeria de fotos simples

---
+++ projeto-galeria/src/index.js
@@ -0,0 +1,11 @@
+//Arquivos SCSS
+
+import './scss/index.scss'
+
+//Dependencias
+import 'jquery'
+import 'bootstrap'
+
+//Arquivos JS
+import './js/core/includes'
+import './js/plugins/cityButtons'
